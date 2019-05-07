73491e6b2f46a7325542a6e03d0099c1b7cfb058
sorz <me@sorz.org>
2019-05-07 17:29:24 +0800

    Allow set bind addr via env var

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ use actix_web::{
     http::Method, server, App, Json, Result, middleware::Logger};
 use serde::Deserialize;
 use env_logger;
+use log::info;
 use sustechcourse::{Course, UserAgent};
 
 #[derive(Deserialize)]
@@ -24,11 +25,16 @@ fn query_course(info: Json<CourseQueryInfo>) -> Result<Json<Vec<Course>>> {
 fn main() {
     std::env::set_var("RUST_LOG", "actix_web=info");
     env_logger::init();
+
+    let bind = std::env::var("HTTP_BIND")
+        .unwrap_or("127.0.0.1:8000".to_string());
+    info!("Start server on {}", bind);
+
     server::new(|| App::new()
             .middleware(Logger::new("%a"))
             .resource("/", |r| r.method(Method::POST).with(query_course))
         )
-        .bind("127.0.0.1:8000")
+        .bind(bind)
         .expect("Can not bind to port 8000")
         .run();
 }
