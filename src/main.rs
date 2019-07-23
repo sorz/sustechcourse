615f8505fd2bb13103a2e7b2aec64e5bf3a4e234
sorz <me@sorz.org>
2019-07-23 15:11:44 +0800

    Migrate to actix-web 1.0

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use actix_web::{
-    http::Method, server, App, Json, Result, middleware::Logger};
+    web, HttpServer, App, Result, middleware::Logger
+};
 use serde::Deserialize;
 use env_logger;
 use log::info;
@@ -12,7 +13,8 @@ struct CourseQueryInfo {
     terms: Option<Vec<(u16, u8)>>,
 }
 
-fn query_course(info: Json<CourseQueryInfo>) -> Result<Json<Vec<Course>>> {
+fn query_course(info: web::Json<CourseQueryInfo>)
+        -> Result<web::Json<Vec<Course>>> {
     let mut agent = UserAgent::new().login(&info.username, &info.password)?;
 
     if let Some(terms) = &info.terms {
@@ -20,9 +22,9 @@ fn query_course(info: Json<CourseQueryInfo>) -> Result<Json<Vec<Course>>> {
         for (year, term) in terms {
             courses.append(&mut agent.query_course(*year, *term)?);
         }
-        Ok(Json(courses))
+        Ok(web::Json(courses))
     } else {
-        Ok(Json(agent.all_courses()?))
+        Ok(web::Json(agent.all_courses()?))
     }
 }
 
@@ -34,11 +36,12 @@ fn main() {
         .unwrap_or("127.0.0.1:8000".to_string());
     info!("Start server on {}", bind);
 
-    server::new(|| App::new()
-            .middleware(Logger::new("%a"))
-            .resource("/", |r| r.method(Method::POST).with(query_course))
-        )
-        .bind(bind)
+    HttpServer::new( ||
+        App::new()
+            .wrap(Logger::default())
+            .service(web::resource("/").route(web::post().to(query_course)))
+    ).bind(bind)
         .expect("Can not bind to port 8000")
-        .run();
+        .run()
+        .expect("Error on running HTTP server")
 }
